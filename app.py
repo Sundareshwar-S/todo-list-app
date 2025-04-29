18feafd0c2378cb4a88293753a6fc737fb3c8d99
Sundareshwar-S <sundareshwar3@gmail.com>
2025-04-29 11:11:28 +0530

    commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
@@ -46,4 +47,5 @@ def delete(id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Get port from Railway
+    app.run(host='0.0.0.0', port=port)
